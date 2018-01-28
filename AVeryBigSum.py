78c87a18f13c3f00127f0c7efe31e7d7818bb6df
nilendra <nini2943@colorado.edu>
2018-01-27 23:12:43 -0800

    Update AVeryBigSum.py

--- AVeryBigSum.py
+++ AVeryBigSum.py
@@ -6,10 +6,19 @@ def aVeryBigSum(n, ar):
     # Complete this function
     #5
     #1000000001 1000000002 1000000003 1000000004 1000000005
+    """
     sum = 0
     for count in range(n):
         sum += ar[count]
     return sum
+    """
+    #def aVeryBigSum(n, ar):
+    return reduce(lambda x, y: x + y, ar, 0)
+
+def reduce(func, arr, s):
+    for v in arr:
+        s = func(s, v)
+    return s
 
 n = int(raw_input().strip())
 ar = map(long, raw_input().strip().split(' '))
